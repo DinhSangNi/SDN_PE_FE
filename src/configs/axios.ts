5f11c06649e0e379a8674f6685983a61d15891f8
TruongDinhSang <sangprotqb@gmail.com>
2025-07-19 01:52:44 +0700

    add api url to axios

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
   withCredentials: true,
 });
 
